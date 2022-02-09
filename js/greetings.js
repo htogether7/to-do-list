19c06208dbfb51f3a7e238a6782ff0b16e13478e
김시환 <sihwan@gimsihwan-ui-MacBookAir.local>
2022-02-10 04:46:24 +0900

    first commit of to-do-list

---
+++ js/greetings.js
@@ -0,0 +1,31 @@
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USER_NAME = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+    localStorage.setItem(USER_NAME, userName);
+    paintGreetings(userName);
+}
+
+function paintGreetings(name) {
+    const username = localStorage.getItem(USER_NAME);
+    greeting.innerText = `Hello ${name}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    }
+
+//loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USER_NAME);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
