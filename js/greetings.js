1ce43ed4f5cba09ade2e573be1bb2747b185220a
김시환 <htogether7@gmail.com>
2022-03-05 20:36:01 +0900

    Feat: link greetings.js to new HTML
    I connect greetings.js to new HTML.
    now, if user submit username, name and today task and week task are saved on
    localStorage.

--- js/greetings.js
+++ js/greetings.js
@@ -1,32 +1,29 @@
-const loginInput = document.querySelector("#login-form input");
-const loginForm = document.querySelector("#login-form");
-const greeting = document.querySelector("#greeting");
-
-const HIDDEN_CLASSNAME = "hidden";
-const USER_NAME = "username";
+const loginInput = document.querySelector(".login-form input");
+const loginForm = document.querySelector(".login-form");
+const greeting = document.querySelector(".greeting");
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginForm.classList.add("hidden");
     const userName = loginInput.value;
-    localStorage.setItem(USER_NAME, userName);
+    localStorage.setItem("name", userName);
     paintGreetings(userName);
     localStorage.setItem("today", "[0,0,0]");
     localStorage.setItem("week", "[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]");
 }
 
 function paintGreetings(name) {
-    const username = localStorage.getItem(USER_NAME);
+    const username = localStorage.getItem("name");
     greeting.innerText = `Hello ${name}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.classList.add("hidden");
     }
 
 //loginForm.addEventListener("submit", onLoginSubmit);
 
-const savedUsername = localStorage.getItem(USER_NAME);
+const savedUsername = localStorage.getItem("name");
 
 if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.classList.remove("hidden");
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     paintGreetings(savedUsername);
